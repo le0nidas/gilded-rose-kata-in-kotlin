b22f0cdca130688585cdc8b4a2f1a69ca900237d
le0nidas <lpartsas@gmail.com>
2018-02-11 10:40:05 +0200

    Refactor UpdateItemQuality to be more readable.

--- gilded-rose-kata/src/main/kotlin/UpdateItemQuality.kt
+++ gilded-rose-kata/src/main/kotlin/UpdateItemQuality.kt
@@ -1,43 +1,35 @@
 fun updateQualityOf(item: Item) {
-	when (item.name) {
-		"Aged Brie" -> updateQualityOfAgedBrie(item)
-		"Backstage passes to a TAFKAL80ETC concert" -> updateQualityOfBackstage(item)
-		"Sulfuras, Hand of Ragnaros" -> updateQualityOfSulfuras(item)
-		else -> updateQualityOfNormal(item)
-	}
-}
-
-private fun updateQualityOfAgedBrie(item: Item) {
-	val value = when {
-		item.quality == 50 -> 0
-		item.sellIn < 0 -> 2
-		else -> 1
+	val value = when (item.name) {
+		"Aged Brie" -> updateValueOfAgedBrie(item)
+		"Backstage passes to a TAFKAL80ETC concert" -> updateValueOfBackstage(item)
+		"Sulfuras, Hand of Ragnaros" -> 0
+		else -> updateValueOfNormal(item)
 	}
 
-	item.quality = if (item.quality + value > 50) 50 else item.quality + value
-}
-
-private fun updateQualityOfBackstage(item: Item) {
-	val value = when {
-		item.quality == 50 -> 0
-		item.sellIn < 0 -> -item.quality
-		item.sellIn <= 5 -> 3
-		item.sellIn <= 10 -> 2
-		else -> 1
+	val valueInLimits = when {
+		item.quality + value > 50 -> 50 - item.quality
+		item.quality + value < 0 -> -item.quality
+		else -> value
 	}
 
-	item.quality = if (item.quality + value > 50) 50 else item.quality + value
+	item.quality = item.quality + valueInLimits
 }
 
-private fun updateQualityOfSulfuras(item: Item) {
-
+private fun updateValueOfAgedBrie(item: Item) = when {
+	item.quality == 50 -> 0
+	item.sellIn < 0 -> 2
+	else -> 1
 }
 
-private fun updateQualityOfNormal(item: Item) {
-	val value = when {
-		item.sellIn < 0 -> 2
-		else -> 1
-	}
+private fun updateValueOfBackstage(item: Item) = when {
+	item.quality == 50 -> 0
+	item.sellIn < 0 -> -item.quality
+	item.sellIn <= 5 -> 3
+	item.sellIn <= 10 -> 2
+	else -> 1
+}
 
-	item.quality = if (item.quality - value < 0) 0 else item.quality - value
+private fun updateValueOfNormal(item: Item) = when {
+	item.sellIn < 0 -> -2
+	else -> -1
 }
